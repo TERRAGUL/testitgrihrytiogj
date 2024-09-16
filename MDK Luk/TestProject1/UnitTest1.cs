17358542621247bc0ac7c4b94f906aecd491250f
terragul <Denislukianov45@gmail.com>
2024-09-16 14:15:28 +0300

    izmenil

--- MDK Luk/TestProject1/UnitTest1.cs
+++ MDK Luk/TestProject1/UnitTest1.cs
@@ -7,37 +7,37 @@ public class UnitTest1
 {
 
         [TestMethod]
-        public void proval1()
+        public void chekaem_pass_fifi_result1()
         {
             Assert.AreEqual(5, Program.chekaem_pass("fifi"));
         }
 
         [TestMethod]
-        public void proval2()
+        public void chekaem_pass_fifififi_result2()
         {
             Assert.AreEqual(5, Program.chekaem_pass("fifififi"));
         }
 
         [TestMethod]
-        public void proval3()
+        public void chekaem_pass_fifIfiif_result3()
         {
             Assert.AreEqual(5, Program.chekaem_pass("fifIfiif"));
         }
 
         [TestMethod]
-        public void proval4()
+        public void chekaem_pass_fifIfiif_result4()
         {
             Assert.AreEqual(5, Program.chekaem_pass("fifIfi!f"));
         }
 
         [TestMethod]
-        public void proval5()
+        public void chekaem_pass_none_result0()
         {
             Assert.AreEqual(5, Program.chekaem_pass(""));
         }
 
         [TestMethod]
-        public void good()
+        public void chekaem_pass_emayEezdesvseest_result5()
         {
             Assert.AreEqual(5, Program.chekaem_pass("emayEe zdes vse est!! 2421"));
         }
